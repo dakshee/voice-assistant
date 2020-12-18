d59ed43b9ba81a19bfd7f4fb6a4212527dce37e5
Dakshayni Awate <69366058+dakshee@users.noreply.github.com>
2020-12-18 15:00:37 +0530

    Add files via upload

---
+++ scratch_2.py
@@ -0,0 +1,14 @@
+import tkinter
+window = tkinter.Tk()
+window.title("PyVA")
+label = tkinter.Label(window, text ="I am PyVA! Command me").pack()
+top_frame = tkinter.Frame(window).pack()
+bottom_frame = tkinter.Frame(window).pack(side = "bottom")
+btn1 = tkinter.Button(top_frame, text = "Command!", fg = "red").pack()
+window.mainloop()
+
+
+
+
+
+
